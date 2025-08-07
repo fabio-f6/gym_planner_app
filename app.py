f2df50b0550e91a7c7be9ad9631708967669a848
fabio-f6 <fabio.fbff@icloud.com>
2025-08-07 17:07:36 +0100

    Update do main functions
    Edit Exercise implemented
    added requirements.txt

--- app.py
+++ app.py
@@ -64,15 +64,42 @@ elif menu == "Add Exercise":
                 st.warning("Please, enter a valid exercise name.")
 
 elif menu == "Edit Exercise":
-    st.subheader("Edit an Existing Exercise")
-    st.info("Feature under development.")
+    if mostrar_exercicios():
+        nomes_exercicios = [f"{i+1}. {ex['Exercise']}" for i, ex in enumerate(st.session_state.exercicios)]
+        escolha = st.selectbox("Select an exercise to edit", nomes_exercicios)
+
+        index = nomes_exercicios.index(escolha)
+        exercicio_atual = st.session_state.exercicios[index]
+
+        with st.form("formulario_exercicio"):
+            st.subheader("Add exercise")
+            new_nome = st.text_input("Name", value = exercicio_atual["Exercise"])
+            new_sets = st.number_input("Sets", min_value=1, max_value=10, step=1, value=exercicio_atual["Sets"])
+            new_reps = st.number_input("Reps", min_value=1, max_value=100, step=1, value=exercicio_atual["Reps"])
+            new_rest = st.number_input("Rest (in seconds)", min_value=1, max_value=600, step=1, value=exercicio_atual["Rest"])
+            new_weight = st.number_input("Weight", min_value=1, max_value=500, step=1, value=exercicio_atual["Weight"])
+            enviado = st.form_submit_button("Save changes")
+
+            if enviado:
+                if new_nome.strip():
+                    st.session_state.exercicios[index] = {
+                        "Exercise": new_nome,
+                        "Sets": new_sets,
+                        "Reps": new_reps,
+                        "Rest": new_rest,
+                        "Weight": new_weight
+                    }
+                    st.success(f"Exercise '{new_nome}' edited successfully.")
+                    st.rerun()
+                else:
+                    st.warning("Please, enter a valid exercise name.")
 
 elif menu == "Remove Exercise":
     if mostrar_exercicios():
 
         indices_para_remover = []
 
-        st.subheader("Select one or more exercises to remove:")
+        st.write("Select one or more exercises to remove:")
         for i, ex in enumerate(st.session_state.exercicios):
             label = f"{ex['Exercise']} (Sets: {ex['Sets']}), (Reps: {ex['Reps']}), (Rest: {ex['Rest']}), (Weight: {ex['Weight']})."
             if st.checkbox(label, key=f"remove_{i}"):
